71f3013a5d32e7d2b219d9923bd71ab65bc207ab
Bryanmartz972 <bryanmartz972@gmail.com>
2022-03-27 11:35:27 -0600

    Declaracion de variables CSS de colores

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import { Provider } from 'react-redux';
 import './App.css';
 
 import store from './Store/index';
-import { Page } from './Components/Views/Page'
+import SignIn from './Components/Views/SignIn';
 
 function App() {
   return (
     <Provider store={store}>
-      <Page/>
+      <SignIn />
     </Provider>
   );
 }
