27c9a55e0aa40091cabbd0614be7655972d16fff
Bryanmartz972 <bryanmartz972@gmail.com>
2022-03-28 10:55:53 -0600

    Creacion de componente de titulo

---
+++ src/Components/UX/Title/Title.js
@@ -0,0 +1,12 @@
+import './Title.css'
+
+const Title = ({ children }) => {
+  return (
+    <div className='title-container'>
+      <h1 className='title'>{children}</h1>
+    </div>
+  )
+}
+
+
+export default Title;
