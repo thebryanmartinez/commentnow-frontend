cc57f96df39bbe988fdc2de3e44994027a5cfdbe
Bryanmartz972 <bryanmartz972@gmail.com>
2022-04-02 12:01:06 -0600

    Implementacion de react-router-dom en App y Header

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import { Provider } from 'react-redux';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 
 import store from './Store/index';
-import SignIn from './Components/Views/SignIn';
+import LogIn from './Components/Views/LogIn';
 import Posts from './Components/Views/Posts';
 
 function App() {
   return (
     <Provider store={store}>
-      <Posts />
+      <BrowserRouter>
+        <Routes>
+        <Route path="/login" element={<LogIn/>}></Route>
+        <Route path="/home" element={<Posts/>}></Route>
+        </Routes>
+      </BrowserRouter>
     </Provider>
   );
 }
--- src/Components/UX/Header/Header.js
+++ src/Components/UX/Header/Header.js
@@ -1,17 +1,19 @@
 import './Header.css'
+import { useSelector } from 'react-redux'
 import { Home, LogOut, User } from 'react-feather'
+import { NavLink, useLocation } from 'react-router-dom'
 
 const Header = () => {
   return (
     <div className='header-container'>
-      <div className="posts--button">
+      <NavLink to='/home' className='posts--button'>
         <Home size={36}></Home>
         <p>Posts</p>
-      </div>
-      <div className="user--button">
+      </NavLink>
+      <NavLink to='/login' className="user--button">
         <User size={36}></User>
         <p>User</p>
-      </div>
+      </NavLink>
       <div className="logout--button">
         <LogOut size={36}></LogOut>
         <p>Logout</p>
